8d7e854c96cb9cce326a76d1f501b3db01ae5867
Binbin Zhang <binbzha@outlook.com>
2018-07-22 19:06:57 +0800

    update works ok

--- dbhelper.py
+++ dbhelper.py
@@ -32,13 +32,23 @@ class DbHelper:
         sql_cmd = '''CREATE TABLE review
                      (id int NOT NULL AUTO_INCREMENT,
                       wav_path text NOT NULL,
+                      valid boolean NOT NULL,
                       status boolean NOT NULL,
                       PRIMARY KEY (id) ) CHARSET=utf8'''
         self.cursor.execute(sql_cmd)
 
-    def insert(self, wav_path, status):
-        sql_cmd = '''INSERT INTO review (wav_path, status)
-                     VALUES ("%s", %s) ''' % (wav_path, status)
+    def insert(self, wav_path, valid, status):
+        sql_cmd = '''INSERT INTO review (wav_path, valid, status)
+                     VALUES ("%s", %s, %s) ''' % (wav_path, valid, status)
+        try:
+            self.cursor.execute(sql_cmd)
+            self.connection.commit()
+        except MySQLdb.Error as e:
+            self.connection.rollback()
+
+    def update(self, id, valid, status):
+        sql_cmd = '''UPDATE review SET valid=%s, status=%s
+                     WHERE id=%d''' % (valid, status, id)
         try:
             self.cursor.execute(sql_cmd)
             self.connection.commit()
@@ -51,7 +61,7 @@ class DbHelper:
         return self.cursor.fetchone()[0]
 
     def get_all_review(self, offset, number):
-        sql_cmd = '''SELECT * FROM review ORDER BY id DESC LIMIT %d,%d''' % (offset, number)
+        sql_cmd = '''SELECT * FROM review ORDER BY id ASC LIMIT %d,%d''' % (offset, number)
         self.cursor.execute(sql_cmd)
         return self.cursor.fetchall()
 
@@ -60,7 +70,7 @@ if __name__ == '__main__':
     db = DbHelper(**CONFIG)
     if not db.table_exist():
         db.create_table()
-    db.insert('a.wav', 'true')
-    db.insert('b.wav', 'false')
+    db.insert('a.wav', 'true', 'true')
+    db.insert('b.wav', 'true', 'false')
     print(db.get_all_review())
 
--- main.py
+++ main.py
@@ -4,6 +4,7 @@
 
 import os
 import socket
+import glob
 import time
 import logging
 import argparse
@@ -19,6 +20,35 @@ class MainHandler(tornado.web.RequestHandler):
     def get(self):
         self.write("Hello, world %s" % socket.gethostname())
 
+class UpdateHandler(tornado.web.RequestHandler):
+    def get(self):
+        id = int(self.get_argument('id', 0, strip=True));
+        operation = self.get_argument('operation', 'true', strip=True);
+        db = dbhelper.DbHelper(**self.application.db_config)
+        db.update(id, operation, 'true')
+
+class AdminHandler(tornado.web.RequestHandler):
+    def get(self):
+        page = int(self.get_argument('page', 0, strip=True))
+        page_items = int(self.get_argument('page_items', 10, strip=True))
+        db = dbhelper.DbHelper(**self.application.db_config)
+        review_count = db.get_review_count()
+        num_pages = int((review_count - 1) / page_items) + 1;
+        if page < 0: page = 0
+        if page >= num_pages: page = num_pages - 1
+        start_page = page - 5
+        if start_page < 0: start_page = 0
+        end_page = page + 5
+        if end_page > num_pages: end_page = num_pages - 1
+
+        items = db.get_all_review(page * page_items, page_items)
+        self.render("static/admin.html", items = items,
+                                         page_items = page_items,
+                                         page = page,
+                                         num_pages = num_pages,
+                                         start_page = start_page,
+                                         end_page = end_page)
+
 class Application(tornado.web.Application):
     def __init__(self):
         logging.basicConfig(level = logging.DEBUG,
@@ -29,17 +59,27 @@ class Application(tornado.web.Application):
         logging.info(settings)
         handlers = [
             (r'/', MainHandler),
+            (r'/admin', AdminHandler),
+            (r'/update', UpdateHandler),
+            (r'/static/', tornado.web.StaticFileHandler, dict(path=settings['static_path']))
         ]
         tornado.web.Application.__init__(self, handlers, **settings)
 
         with open(FLAGS.config_file, "r") as fid:
             self.config = json.load(fid)
         self.db_config = self.config["db"]
-        # Wait mysql to start service
-        time.sleep(5)
+        # wait mysql to start service
+        logging.info('waiting mysql be full ready, 20s')
+        time.sleep(20)
         db = dbhelper.DbHelper(**self.db_config)
         if not db.table_exist():
             db.create_table()
+            # update index
+            all_wav_files = glob.glob(r'wav/*/*.wav')
+            for i, wav in enumerate(all_wav_files):
+                db.insert(wav, 'false', 'false')
+                if i % 50 == 0:
+                    logging.info('creating index %d', i)
 
     def start(self):
         logging.info('server start, port: %d...' % 8000)
